3e1535881410854e15b8a2a5c86f2d43ca21d9c2
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-11-13 17:39:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,16 +16,22 @@ def exec_code_in_venv(
         "linux": "source {}/bin/activate",
         "darwin": "source {}/bin/activate",
     }
+    VENV_JOIN_CMD_CHAR_BY_OS = {
+        "win32": "&&",
+        "linux": "&",
+        "darwin": "&",
+    }
     requirements_path = os.path.join(root_folder, "requirements.txt")
     if os.path.exists(requirements_path):
         pip_install_cmd = f"pip install -r " + requirements_path
     else:
         pip_install_cmd = ""
     activate_cmd = VENV_ACTIVATE_CMD_BY_OS[sys.platform].format(venv_name)
-    cmd = f"{activate_cmd} && {pip_install_cmd} && {cmd}"
+    join_cmd_char = VENV_JOIN_CMD_CHAR_BY_OS[sys.platform]
+    cmd = f"{activate_cmd} {join_cmd_char} {pip_install_cmd} {join_cmd_char} {cmd}"
     venv_path = os.path.join(root_folder, venv_name)
     if not os.path.exists(venv_path):
-        cmd = f"python -m venv {venv_path} && {cmd}"
+        cmd = f"python -m venv {venv_path} {join_cmd_char} {cmd}"
     process = subprocess.Popen(
         cmd,
         stdin=subprocess.PIPE,
@@ -52,11 +58,6 @@ def main(root_folder: str):
 
 
 if __name__ == "__main__":
-    # main(f"./soumissions/exemple_soumission")
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
-        for solution in os.listdir("./soumissions"):
-            # if solution == "RandomTeam":
-            # 	continue
-            print(f"Testing solution: {solution}")
-            main(f"./soumissions/{solution}")
+    solution_folder = "./RandomTeam"
+    print(f"Testing solution: {solution_folder}")
+    main(os.path.join("./soumissions", solution_folder))
